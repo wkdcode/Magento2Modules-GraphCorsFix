e87df76f75984d4c526b8593f1aa662496a3f9d0
stratus21-liam <liam.c@stratus21.com>
2024-08-30 16:45:43 +0100

    added module config for graphCorsFix

--- Plugin/QueryParserPluginString.php
+++ Plugin/QueryParserPluginString.php
@@ -4,9 +4,25 @@ namespace Stratus21\GraphCorsFix\Plugin;
 
 use GraphQL\Language\AST\DocumentNode;
 use Magento\Framework\GraphQl\Query\QueryParser;
+use Psr\Log\LoggerInterface;
 
 class QueryParserPluginString
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor
+     *
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * After plugin to modify the result of the parse method
      *
@@ -17,6 +33,11 @@ class QueryParserPluginString
      */
     public function afterParse(QueryParser $subject, DocumentNode $result, string $query)
     {
+        // Log the contents of the definitions array
+        $this->logger->info('GraphQL DocumentNode definitions:', [
+            'definitions' => $result->definitions
+        ]);
+
         // Check if the DocumentNode's definitions are empty
         if (empty($result->definitions)) {
             // Return an empty string if definitions are empty
