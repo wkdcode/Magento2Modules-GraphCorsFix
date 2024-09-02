7cfcb2c22bab49c551001891a68f4580db11db6e
stratus21-liam <liam.c@stratus21.com>
2024-09-02 10:59:36 +0100

    added module config for graphCorsFix

--- Plugin/QueryParserPluginString.php
+++ Plugin/QueryParserPluginString.php
@@ -23,32 +23,39 @@ class QueryParserPluginString
         $this->responder            = $responder;
     }
 
-    /**
-     * After plugin to modify the result of the parse method
-     *
-     * @param QueryParser $subject
-     * @param DocumentNode $result
-     * @param string $query
-     * @return DocumentNode|string
-     */
-    public function afterParse(QueryParser $subject, DocumentNode $result, string $query)
-    {
-         // Log to check if the plugin is working
-         $this->responder->log('LIAM', 'info', 'afterParse working');
 
-         // Check if the query was empty (fall back to minimal query) and if the DocumentNode's definitions are empty
-         if (empty($query)) {
-             // Log the condition
-             $this->responder->log('LIAM', 'info', 'Empty query and empty definitions, returning an empty string');
- 
-             // Return an empty string
-             return '';
-         }
- 
-         // Log the result before returning
-         $this->responder->log('LIAM', 'info', 'Returning original DocumentNode');
- 
-         // Otherwise, return the DocumentNode as is
-         return $result;
+    public function aroundParse(QueryParser $subject, callable $proceed, string $query)
+    {
+        $this->responder->log('LIAM', 'info', 'aroundParse before result');
+
+        // Modify method behavior
+        $result = $proceed($query);
+
+        $this->responder->log('LIAM', 'info', 'aroundParse after result');
+
+
+        // Manipulate result if needed
+        return $result;
     }
+
+    // public function afterParse(QueryParser $subject, DocumentNode $result, string $query)
+    // {
+    //      // Log to check if the plugin is working
+    //      $this->responder->log('LIAM', 'info', 'afterParse working');
+
+    //      // Check if the query was empty (fall back to minimal query) and if the DocumentNode's definitions are empty
+    //      if (empty($query)) {
+    //          // Log the condition
+    //          $this->responder->log('LIAM', 'info', 'Empty query and empty definitions, returning an empty string');
+ 
+    //          // Return an empty string
+    //          return '';
+    //      }
+ 
+    //      // Log the result before returning
+    //      $this->responder->log('LIAM', 'info', 'Returning original DocumentNode');
+ 
+    //      // Otherwise, return the DocumentNode as is
+    //      return $result;
+    // }
 }
